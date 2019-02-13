01d8f815ab4936dd1aa66d0951b9f5c43d82bb7e
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-02-13 16:22:35 +0100

    delete comments

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import Friend from './Friend';
 
 
-// Implement `Friends` component so it takes an array of friends,
-// each friend being an object like so: { name: 'Peter' },
-// and renders a list of friends' names.
-// Wrap everything in a div with the class of 'sub-container' so styles will work.
 export default function Friends(props) {
   return (
     <div className="sub-container">
