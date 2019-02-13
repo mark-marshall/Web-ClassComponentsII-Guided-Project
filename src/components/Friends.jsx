579fa8e714e63ddc4e87303093fd9b6e620d1a6d
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-02-13 16:26:46 +0100

    Wip

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import Friend from './Friend';
 
 
-export default function Friends(props) {
+export default function Friends({ friends, setCurrentFriend, deleteFriend }) {
   return (
     <div className="sub-container">
       <h3>These are my friends:</h3>
       {
-        !props.friends.length && <h5>No friends. Sad!</h5>
+        !friends.length && <h5>No friends. Sad!</h5>
       }
       {
-        props.friends.map(friend => (
+        friends.map(friend => (
           <Friend
             key={friend.id}
             friend={friend}
-            deleteFriend={props.deleteFriend}
-            setCurrentFriend={props.setCurrentFriend}
+            deleteFriend={deleteFriend}
+            setCurrentFriend={setCurrentFriend}
           />
         ))
       }
