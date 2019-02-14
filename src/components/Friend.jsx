6fa62a15fe87e0d1f850a5f9048f40c5cbd8023d
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-02-14 11:23:34 +0100

    we can delete

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 
 export default function Friend({ friend, deleteFriend, setCurrentFriendId }) {
   // `deleteFriend` and `setCurrentFriendId` are not going to work in those onClick handlers...
+  const onDelete = () => deleteFriend(friend.id);
+  const onSetCurrentFriendId = () => setCurrentFriendId(friend.id);
 
   return (
     <div>
       <span>{friend.name} is {friend.age}</span>
 
-      <button onClick={setCurrentFriendId} className='small'>Edit Friend</button>
-      <button onClick={deleteFriend} className='small danger'>Delete Friend</button>
+      <button onClick={onSetCurrentFriendId} className='small'>Edit Friend</button>
+      <button onClick={onDelete} className='small danger'>Delete Friend</button>
     </div>
   );
 }
